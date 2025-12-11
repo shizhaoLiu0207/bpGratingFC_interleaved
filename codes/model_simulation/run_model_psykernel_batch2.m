520f1aaa22e35580b09bd3f7ecf6d7150432ff60
shizhaoliu0207 <sliu88@ur.rochester.edu>
2025-12-11 09:54:56 -0500

    Misc update from Linux

--- codes/model_simulation/run_model_psykernel_batch2.m
+++ codes/model_simulation/run_model_psykernel_batch2.m
@@ -15,15 +15,15 @@ end
 number_samples_per_evidence         = 6;
 stimulus_regime                     = 'dynamic-switching-signal-blocked';
 
-prior_task_list                     = {[1, 0]};
-image_task_list                     = {'cardinal'};
+prior_task_list                     = {[1, 0];[0.7, 0.3];[0.5, 0.5]; [0.3,0.7]};
+image_task_list                     = {'cardinal';'cardinal';'cardinal';'cardinal'};
 
 
 nRepeats                            = 1000;
 run_ori_energy                      = true;
 n_ori_bin                           = 12;
 
-stimulus_contrast_list              = {[0,3],[3,0],[0,6],[6,0]};
+stimulus_contrast_list              = {[0,6],[6,0]};
 nRepeats_list                       = ones(size(stimulus_contrast_list)) * nRepeats;
 
 runOptions.run_ori_energy           = true;
@@ -31,6 +31,6 @@ runOptions.n_ori_bin                = 12;
 runOptions.task_mode                = 'single';
 runOptions.clamp_prior              = true;
 runOptions.save_folder              = fullfile(home_folder, 'moreRepeats');
-runOptions.nSession                 = 50; %%% run 10 sessions per condition
+runOptions.nSession                 = 20; %%% run 10 sessions per condition
 
 generate_data_model_psyKernel(prior_task_list, image_task_list, stimulus_contrast_list, nRepeats_list, runOptions)
